19e4f21373b1051df262e22712dffadb4f3c2a00
Nkhil Katekhaye <katekhaye.nikhil@gmail.com>
2021-05-02 19:44:41 +0530

    Changes done for dark theme demo

---
+++ app/src/main/java/com/nikhil/dark_theme_demo/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.nikhil.dark_theme_demo
+
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn : Button = findViewById(R.id.btn_switch)
+        btn.setOnClickListener {
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_YES ->
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                Configuration.UI_MODE_NIGHT_NO ->
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+    }
+}
+
